9dc5634fccb2afe767944a5f802f1cfad74754de
Ishan Rangnekar <72988033+WhiteStudent@users.noreply.github.com>
2021-04-02 17:08:12 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-hahahahaha
+function setup() {
+    canvas = createCanvas(550, 450);
+    canvas.position(560, 5);
+
+    video = createCapture(VIDEO)
+    video.size(550, 500)
+
+    posenet = ml5.poseNet(video, modelLoaded)
+    posenet.on("pose", gotPoses)
+}
+
+function draw() {
+    background("grey")
+}
+
+function modelLoaded() {
+    console.log("Model loaded");
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+    }
+}
