1e495fc2fc45a47fd3b5cfda6dcf31ef3ec5e221
Ishan Rangnekar <72988033+WhiteStudent@users.noreply.github.com>
2021-04-06 18:04:50 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+nose_x = 0
+nose_y = 0
+left_wrist_x = 0
+right_wrist_x = 0
+difference = 0
+
 function setup() {
     canvas = createCanvas(550, 450);
     canvas.position(560, 5);
@@ -11,6 +17,10 @@ function setup() {
 
 function draw() {
     background("grey")
+    textSize(difference)
+    fill("0, 0, 255")
+    text("Ishan",)
+    document.getElementById("square_side").innerHTML = "The size of the text will be " + difference
 }
 
 function modelLoaded() {
@@ -20,5 +30,13 @@ function modelLoaded() {
 function gotPoses(results) {
     if (results.length > 0) {
         console.log(results);
+        nose_x = results[0].pose.nose.x
+        nose_y = results[0].pose.nose.y
+        console.log("Nose x = " + nose_x + ", Nose y = " + nose_y);
+        left_wrist_x = results[0].pose.leftWrist.x
+        right_wrist_x = results[0].pose.rightWrist.x
+        difference = floor(left_wrist_x - right_wrist_x)
+        console.log("Left Wrist x = " + left_wrist_x + ", Right Wrist x = " + right_wrist_x);
+        console.log(difference);
     }
 }
